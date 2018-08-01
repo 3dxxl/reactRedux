0310ad9def0ff9bcedd0a392cd287880cd5eca21
Binjam Gebre <37068641+3dxxl@users.noreply.github.com>
2018-08-01 16:18:40 +0200

    Erklärung verbessert

--- erklärung.js
+++ erklärung.js
@@ -1,18 +1,18 @@
 /*
 
-Nach dem man npm install redux & npm install react-redux ausgeführt hat geht es los mit.
+Nach dem man npm install redux & npm install react-redux ausgeführt hat geht es los.
 
 
 --Den Store erstellen--
 
-Muß man eine Store Datei erstellen welche den Zustand aller Componente Verwaltet z.b.:
+Zuerst erstellt du eine Store Datei welche den Zustand aller Componente Verwaltet z.b.:
 MeinStore.js
 
-In dieser Datei importier man den store von redux:
+In dieser Datei importierst du den store von redux:
 import {createStore} from 'redux';
 
-Dann erstellt man einen initialen Zustand welches ein oder mehrere Objekte beinhaltet
-und übergibt diesem Objekt/Objekte einen Wert:
+Dann erstellst du einen initialen Zustand welches ein oder mehrere Objekte beinhaltet
+und übergibst diesem Objekt/Objekte einen Wert:
 export const initialState = {
     farbe: 'green'
 };
@@ -27,13 +27,13 @@ den reducer und den initialState:
 export const store = createStore(reducer, initialState);
 
 
---Den Reducer erstelen z.b. MeinReducer.js--
+--Den Reducer erstellen z.b. MeinReducer.js--
 
 Als erstes importieren wir den initialenState aus der Datei MeinStore.js:
 import {initialState} from './MeinStore';
 
 
-Dann erstellen wir einen reducer, dieser beinhaltet de Zustand & die action,
+Dann erstellen wir einen reducer, dieser beinhaltet den Zustand & die action,
 dem Zustand wird der initialeState übergeben, mit Switch geben wir die jeweiligen Einstellungen
 weiter und zum Schluß
 wird nochmal der state zurückgegeben:
@@ -92,7 +92,7 @@ registerServiceWorker();
 
 
 --Daten aus dem Redux Store extrahieren und in die Container Componente über die Props einfügen u.
-creatirAction importieren--
+actionCreators importieren--
 
 Zuerst muss  ich connect von React Redux importieren, danach die actionCreators:
 import {connect} from 'react-redux';
@@ -109,7 +109,7 @@ WICHTIG: Diese müssen wir in der index.js Datei einfügen:
 
 
 Dann erstellen wir eine Funktion mapStateToProps, diese holt sich den gesamten Zustand aus
-dem Redux Store u. holt sich einen Teil heraus:
+dem Redux Store u. gibt einen Teil zurück:
  function mapStateToProps (state) {
 
     return {farbe: state.farbe};
@@ -122,6 +122,11 @@ dem Redux Store u. holt sich einen Teil heraus:
 
 
 Und dann importieren wir die props in die Events:
+<button style={{margin: 20}}
+            onClick={this.props.nameOffTheAction}
+            >Ändere die Farbe mit Redux
+</button>
+
 <button style={{margin: 20}}
             onClick={this.props.nameOffTheActionZwei}
             >Ändere die Farbe mit Redux
