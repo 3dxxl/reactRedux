9aaaa48767ab72a7c7340e5e60216400002cbe4b
Binjam Gebre <info@react-entwickler.de>
2018-08-03 15:25:41 +0200

    eine PDF Erklärung hinzugefügt

--- src/MeinStore.js
+++ src/MeinStore.js
@@ -9,7 +9,7 @@ export const initialState = {
     farbe: 'green'
 };
 
-export const store = createStore(reducer, initialState);
+export const store = createStore(reducer, initialState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 
 
